96f71d32090babc06f15d20592f7acd13835c2b8
L3to <lf2006@hotmail.com.br>
2024-03-19 20:29:34 -0300

    jogo

---
+++ pythonjogo.py
@@ -0,0 +1,20 @@
+import random
+
+num = random.randint(1, 1000)
+chance = 10
+
+while chance != 0:
+    tentativa = int(input("Tente advinhar um número: "))
+    if tentativa == num: 
+        break
+    elif tentativa > num:
+        print("O número é menor")
+    elif tentativa < num:
+        print("O número é maior")
+    chance -= 1
+
+if chance == 0:
+    print("Você não tem mais chances")
+else:
+    print("Parabéns esse é o número!")
+    
