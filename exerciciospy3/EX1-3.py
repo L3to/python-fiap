b32ef55c1c7077b24b607580b189fe5f8f3650cc
L3to <lf2006@hotmail.com.br>
2024-04-03 11:06:02 -0300

    correção 1

--- exerciciospy3/EX1-3.py
+++ exerciciospy3/EX1-3.py
@@ -1,7 +1,8 @@
 
-contador = 0
+pares = 0
+n = int(input("Informe um numero: "))
 while n != 0:
-    n = int(input("Informe um numero: "))
     if n % 2 == 0:
-        contador += n
-print(f'A soma dos números pares é {contador}')
+        pares += n
+    n = int(input("Informe um numero: "))
+print(f'A soma dos números pares é {pares}')
