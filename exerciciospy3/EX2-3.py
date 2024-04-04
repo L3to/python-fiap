76b90818631e30dd47706b40089aa10a08b13cfe
Luiz <160269549+L3to@users.noreply.github.com>
2024-04-04 19:11:51 -0300

    Correção Exercício 2

--- exerciciospy3/EX2-3.py
+++ exerciciospy3/EX2-3.py
@@ -1,9 +1,11 @@
 n = int(input("Informe o número de alunos: "))
 contador = 0
 media = 0
-
+while n <= 0:
+    n = int(input("Valor inválido, digite novamente: "))
+    
 while contador < n:
-    nota = int(input("Informe a nota do aluno: "))
+    nota = float(input("Informe a nota do aluno: "))
     contador += 1
     media += nota
     
